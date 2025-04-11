81d502f87ebc38ad6a43fb398838165e795454ec
RitneshThakur <thakurritnesh@gmail.com>
2025-04-11 21:50:13 +0545

    used git ignorse

--- lib/constants/app_constants.dart
+++ lib/constants/app_constants.dart
@@ -1,5 +1,4 @@
-String apiKey = "17a55592b7dee41dfd6c20bd4d305177";
-String appId = "67c8ff96";
+import 'api.dart';
 
 String generateUrl(String query) {
   return "https://api.edamam.com/search?q=$query&app_id=$appId&app_key=$apiKey";
